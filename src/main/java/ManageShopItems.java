ed989c91f7160127ee0e29ecfe45ce46f39bbbff
ZuwinaAlgh <z.alghafri@hotmail.com>
2023-01-30 10:36:07 +0400

    add items in item table

---
+++ src/main/java/ManageShopItems.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class ManageShopItems {
+
+	 public static void addItems() throws IOException, InterruptedException{
+ 		
+ 		String url = "jdbc:sqlserver://localhost:1433;databaseName=InvoicingSystem;encrypt=true;trustServerCertificate=true";
+         String user = "sa";
+         String pass = "root";
+         
+         Scanner sa=new Scanner(System.in);
+         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+         
+         System.out.println("...Enter ITEMS Details...:");
+         System.out.println("...How Many Rows You Want To Add...:");
+         int rows=sa.nextInt();
+         for(int i=0; i<=rows;i++) {
+         System.out.println("Enter Item Name:");
+         String Item_Name =bf.readLine();
+         System.out.println("Enter Unit price:");
+         float Unit_price=sa.nextFloat();
+         System.out.println("Enter Quantity:");
+         int Quantity=sa.nextInt();
+         System.out.println("Enter Quantity Amount:");
+         float Quantity_Amount=sa.nextFloat();
+         
+         
+         
+         String sql="insert into ITEMS(Item_Name, Unit_price, Quantity, Quantity_Amount)VALUES(?,?,?,?)";
+         
+         Connection con = null;
+
+         try {
+
+             Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+             DriverManager.registerDriver(driver);
+
+             con = DriverManager.getConnection(url, user, pass);
+             PreparedStatement ps=con.prepareStatement(sql);
+             
+             ps.setString(1, Item_Name);
+             ps.setFloat(2, Unit_price);
+             ps.setInt(3, Quantity);
+             ps.setFloat(4, Quantity_Amount);
+             ps.executeUpdate();
+
+                 System.out.println( "insert data successfully");
+         }
+
+         catch (Exception ex) {
+             System.err.println(ex);
+         }
+         }}
+		
+
+	}
+
+
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -12,87 +12,85 @@ public class Menu {
 	
 	
 	
-	//Main menu
-	
-		while (mainMenu) {
-			System.out.println("###WELCOME TO GROCERIES shop SYSTEM###");
-			System.out.println("###Please choose one option........###");
-			System.out.println("1.--Shop Settings--");
-			System.out.println("2.--Manage Shop Items--");
-			System.out.println("3.--Create New Invoice--");
-			System.out.println("4.--Statistics Report--");
-			System.out.println("5.--Report All Invoices--");
-			System.out.println("6.--Search One Invoice--");
-			System.out.println("7.--Program Statistics--");
-			System.out.println("8.--Exit--");
-			System.out.println("###............THANK YOU............###");
-			String menu = sa.next();
-			int option = Integer.parseInt(menu);
-			switch (option) {
-			case 1:
+//	//Main menu
+//	
+//		while (mainMenu) {
+//			System.out.println("###WELCOME TO GROCERIES shop SYSTEM###");
+//			System.out.println("###Please choose one option........###");
+//			System.out.println("1.--Shop Settings--");
+//			System.out.println("2.--Manage Shop Items--");
+//			System.out.println("3.--Create New Invoice--");
+//			System.out.println("4.--Statistics Report--");
+//			System.out.println("5.--Report All Invoices--");
+//			System.out.println("6.--Search One Invoice--");
+//			System.out.println("7.--Program Statistics--");
+//			System.out.println("8.--Exit--");
+//			System.out.println("###............THANK YOU............###");
+//			String menu = sa.next();
+//			int option = Integer.parseInt(menu);
+//			switch (option) {
+//			case 1:
 //				
+//				break;
+//
+//			case 2:
+//
+//				break;
+//			case 3:
 //				
+//				break;
+//			case 4:
 //				
-				
-				break;
-
-			case 2:
-
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-
-			case 5:
-				
-				break;
-			case 6:
-
-				break;
-			case 7:
-				break;
-			case 8:
-				
-			}
-
-		}
-		mainMenu = false;
-		
-		//shop Setting menu
-		while (ShopSettingMenu) {
-			System.out.println("###..........SHOP SETTING..........###");
-			System.out.println("###Please choose one option........###");
-			System.out.println("1.--Load Data (invoices)--");
-			System.out.println("2.--Load Data (Items)--");
-			System.out.println("3.--Set Shop Name--");
-			System.out.println("4.--Set Invoice Header--");
-			System.out.println("5.--Go Back To MAIN MENU--");
-			System.out.println("###............THANK YOU............###");
-			String ShopSettingMenu1 = sa.next();
-			int option = Integer.parseInt(ShopSettingMenu1);
-			switch (option) {
-			case 1:
-				ShopSetting.insertdata();
-				break;
-			case 2:
-				ShopSetting.insertItemdata();
-				break;
-			case 3:
-				ShopSetting.insertShopdata();
-				break;
-			case 4:
-				ShopSetting.insertInvoiceHeaderdata();
-				break;
-			case 5:	
-				ShopSettingMenu=false;
-				mainMenu=true;
-			}
-
-		}
-		
+//				break;
+//
+//			case 5:
+//				
+//				break;
+//			case 6:
+//
+//				break;
+//			case 7:
+//				break;
+//			case 8:
+//				
+//			}
+//
+//		}
+//		mainMenu = false;
+//		
+//		//shop Setting menu
+//		
+//		while (ShopSettingMenu) {
+//			System.out.println("###..........SHOP SETTING..........###");
+//			System.out.println("###Please choose one option........###");
+//			System.out.println("1.--Load Data (invoices)--");
+//			System.out.println("2.--Load Data (Items)--");
+//			System.out.println("3.--Set Shop Name--");
+//			System.out.println("4.--Set Invoice Header--");
+//			System.out.println("5.--Go Back To MAIN MENU--");
+//			System.out.println("###............THANK YOU............###");
+//			String ShopSettingMenu1 = sa.next();
+//			int option = Integer.parseInt(ShopSettingMenu1);
+//			switch (option) {
+//			case 1:
+//				ShopSetting.insertdata();
+//				break;
+//			case 2:
+//				ShopSetting.insertItemdata();
+//				break;
+//			case 3:
+//				ShopSetting.insertShopdata();
+//				break;
+//			case 4:
+//				ShopSetting.insertInvoiceHeaderdata();
+//				break;
+//			case 5:	
+//				ShopSettingMenu=false;
+//				mainMenu=true;
+//			}
+//
+//		}
+//			
 		
 		// MANAGE MENU
 		while (ManageShopMenu) {
@@ -108,7 +106,7 @@ public class Menu {
 			int option = Integer.parseInt(ManageShopMenu1);
 			switch (option) {
 			case 1:
-				
+				ManageShopItems.addItems();
 				break;
 
 			case 2:
