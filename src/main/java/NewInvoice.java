767c2be0dfaebd94a51d0211c5d4f0dfdec4ae0c
ZuwinaAlgh <z.alghafri@hotmail.com>
2023-01-31 09:37:39 +0400

    create new invoice

--- src/main/java/NewInvoice.java
+++ src/main/java/NewInvoice.java
@@ -2,52 +2,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Random;
 import java.util.Scanner;
 
 public class NewInvoice{
 
 		public static void createNewInvoice() throws IOException, InterruptedException{
 			String url = "jdbc:sqlserver://localhost:1433;databaseName=InvoicingSystem;encrypt=true;trustServerCertificate=true";
-			String user = "sa";
-			String pass = "root";
-			Connection con;
+	        String user = "sa";
+	        String pass = "root";
+	        
+	        Scanner sa=new Scanner(System.in);
+	        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+	        
+	        System.out.println("...Create New Invoice...:");
+	        System.out.println("...How Many Invoice You Want To Enter...:");
+	        int rows=sa.nextInt();
+	        for(int i=0; i<=rows;i++) {
+	        System.out.println("Enter Customer_Full_Name:");
+	        String Customer_Full_Name =bf.readLine();
+	        System.out.println("Enter Customer_Phone_Number:");
+	        int Phone_Number=sa.nextInt();
+	        System.out.println("Enter Invoice_Date:");
+	        String Invoice_Date=sa.next();
+	        System.out.println("Enter Number_Of_Items:");
+	        int Number_Of_Items=sa.nextInt();
+	        System.out.println("Enter Total_Amount:");
+	        float Total_Amount=sa.nextFloat();
+	        System.out.println("Enter Paid_Amount:");
+	        float Paid_Amount=sa.nextFloat();
+	        System.out.println("Enter Balance:");
+	        float Balance=sa.nextFloat();
+	        
+	        
+	        
+	        String sql="insert into CUSTOMER_INVOICE(Customer_Full_Name,Phone_Number,Invoice_Date,Number_Of_Items,Total_Amount,Paid_Amount,Balance)VALUES(?,?,?,?,?,?,?)";
+	        
+	        Connection con = null;
 
-			try {
-				Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-				DriverManager.registerDriver(driver);
+	        try {
 
-				con = DriverManager.getConnection(url, user, pass);
+	            Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+	            DriverManager.registerDriver(driver);
 
-				Statement st = con.createStatement();
+	            con = DriverManager.getConnection(url, user, pass);
+	            PreparedStatement ps=con.prepareStatement(sql);
+	            
+	            ps.setString(1, Customer_Full_Name);
+	            ps.setInt(2, Phone_Number);
+	            ps.setString(3, Invoice_Date);
+	            ps.setInt(4, Number_Of_Items);
+	            ps.setFloat(5, Total_Amount);
+	            ps.setFloat(6, Paid_Amount);
+	            ps.setFloat(7, Balance);
+	            ps.executeUpdate();
 
-				String sql = "select * from CUSTOMER_INVOICE INNER JOIN ITEMS ON ITEMS.Item_Id=CUSTOMER_INVOICE.Item_Id INNER JOIN Shop ON Shop.Shop_Id=ITEMS.Shop_Id INNER JOIN InvoiceHeader ON InvoiceHeader.Header_id=Shop.Header_id  where CUSTOMER_INVOICE.Customer_Id=1";
-				ResultSet rs = st.executeQuery(sql);
-				while (rs.next()) {
-					String Customer_Full_Name = rs.getString("Customer_Full_Name");
-					int Phone_Number= rs.getInt("Phone_Number");
-					String Invoice_Date=rs.getString("Invoice_Date");
-					float Total_Amount=rs.getFloat("Total_Amount");
-					String Item_Name=rs.getString("Item_Name");
-					int Quantity=rs.getInt("Quantity");
-					String Shop_Name =rs.getString("Shop_Name");
-					int Tel=rs.getInt("Tel");
-					int Fax=rs.getInt("Fax");
-					String Email=rs.getString("Email");
-					String Website=rs.getString("Website");
-					
-					System.out.println(Customer_Full_Name,Phone_Number,Invoice_Date,Total_Amount,Item_Name,Quantity,Shop_Name,Tel,Fax,Email,Website);
-				}
+	                System.out.println( "insert data successfully");
+	        }
 
-			} catch (Exception ex) {
-				
-				System.err.println(ex);
-			}
+	        catch (Exception ex) {
+	            System.err.println(ex);
+	        }
 	 		
-	}}
+	}}}
 
 
